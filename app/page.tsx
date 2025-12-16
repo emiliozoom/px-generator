81725faace5e79f154b87d78b08d32835c83c116
emiliozoom <emiliozoom73@gmail.com>
2025-12-16 17:54:36 -0600

    add home page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import PixelGridCanvas from "@/components/PixelGridCanvas";
+
+export default function Home() {
+  return (
+    <main style={{ padding: 32, display: "flex", gap: 20 }}>
+      <PixelGridCanvas />
+    </main>
+  );
+}
