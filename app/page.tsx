65a36f5b98bf55008e52858f36454cfd7f89522a
emiliozoom <emiliozoom73@gmail.com>
2025-12-16 18:07:03 -0600

    Adjust main container padding
    Reduced padding in the main container from 32 to 16.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import PixelGridCanvas from "@/components/PixelGridCanvas";
 
 export default function Home() {
   return (
-    <main style={{ padding: 32, display: "flex", gap: 20 }}>
+    <main style={{ padding: 16 }}>
+      <h1>PX Generator v1.1</h1>
       <PixelGridCanvas />
     </main>
   );
